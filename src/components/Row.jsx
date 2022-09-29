41ae9be0873913385660e3a8860bc71f9913a45b
PedroWharton <pedrowharton@gmail.com>
2022-09-29 12:37:28 -0300

    avanzo carrusel scroll

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../axios.js';
 import '../stylesheets/components/Row.css';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const baseURL = 'https://image.tmdb.org/t/p/original'
 
@@ -17,20 +19,34 @@ function Row({title, fetchURL, isLargeRow}) {
 
     }, [fetchURL])
 
+    const slideLeft = () => {
+        let slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRight = () => {
+        let slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
+
 
     
 
   return (
     <div className='row'>
         <h2>{title}</h2>
-        <div className='row-posters'>
-            {movies?.map(movie => (
-                <img
-                key={movie.id}
-                className={`row-poster ${isLargeRow && 'row-posterLarge'}`} 
-                src={baseURL + (isLargeRow ? movie.poster_path: movie.backdrop_path)} 
-                alt={movie.name}/>
-            ))}
+        <div className="slider-container">
+            <ArrowBackIosNewIcon className='slider-icon' onClick={slideLeft}/>
+            <div className='row-posters' id='slider'>
+                {movies?.map(movie => (
+                    <img
+                    key={movie.id}
+                    className={`row-poster ${isLargeRow && 'row-posterLarge'}`} 
+                    src={baseURL + (isLargeRow ? movie.poster_path: movie.backdrop_path)} 
+                    alt={movie.name}/>
+                ))}
+            </div>
+            <ArrowForwardIosIcon className='slider-icon' onClick={slideRight}/>
         </div>
     </div> 
   )
