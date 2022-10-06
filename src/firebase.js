7cba8fe6d5ba3502160f4e38444034d30d2e0aaf
PedroWharton <pedrowharton@gmail.com>
2022-10-06 17:12:01 -0300

    updates

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGWcBTYazqE3jY5ANBjOecTKDgXN0-uQE",
+    authDomain: "bromteck-template.firebaseapp.com",
+    projectId: "bromteck-template",
+    storageBucket: "bromteck-template.appspot.com",
+    messagingSenderId: "170584540291",
+    appId: "1:170584540291:web:fe8475b3b81a5be9b1c7b6"
+};
